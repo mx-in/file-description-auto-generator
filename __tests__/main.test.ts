d8011757294b69ca354cdb42afed4b423a9809f7
mx_in <mouxin1993@gmail.com>
2023-05-19 18:59:09 +0800

    chore: inputs check

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,10 +1,9 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
-import {expect, test} from '@jest/globals'
+// import {wait} from '../src/wait'
+// import * as process from 'process'
+import {test} from '@jest/globals'
+import {run} from '../src/main'
 
-test('throws invalid number', async () => {
+/* test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
   await expect(wait(input)).rejects.toThrow('milliseconds not a number')
 })
@@ -15,15 +14,9 @@ test('wait 500 ms', async () => {
   const end = new Date()
   const delta = Math.abs(end.getTime() - start.getTime())
   expect(delta).toBeGreaterThan(450)
-})
+}) */
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+  run()
 })
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,30 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
-import {expect, test} from 'jest/globals'
 
-async function run(): Promise<void> {
+// openai-api-key:
+//   description: 'Your openai api key'
+//   required: true
+// openai-prompt:
+//   description: 'The OpenAI prompt'
+//   require: true
+// model:
+//   description: 'The model to use'
+//   required: false
+//   default: 'text-davinci-003'
+// source-dist-map:
+export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    core.debug('start to run : )')
+    const api_key = core.getInput('openai-api-key')
+    core.debug(`api_key: ${api_key}`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const prompt = core.getInput('openai-prompt')
+    core.debug(`prompt: ${prompt}`)
+
+    const model = core.getInput('model')
+    core.debug(`model: ${model}`)
+
+    const source_dist_map = core.getInput('source-dist-map')
+    core.debug(`source_dist_map: ${source_dist_map}`)
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
